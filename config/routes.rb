73d4917adc55357d5f6681c8682b5385d55de62e
Diana Pinon <dianapinonb@gmail.com>
2020-11-26 12:29:40 -0700

    Removed unnecessary models and controllers and created club_books

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-
-
   resources :clubs do
     resources :club_memberships, only: [ :new, :create, :edit, :update ]
     resources :rooms do
       resources :room_messages
     end
+    resources :club_books, only: [ :new, :create, :edit, :update ]
   end
 
   resources :books, only: :show
   resources :club_memberships, only: :destroy
+  resources :club_books, only: :destroy
 end
