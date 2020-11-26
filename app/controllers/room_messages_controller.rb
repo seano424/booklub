ed57c2218cf8e415b0c32fc3b1b3466d084075c3
Lauren Smalley <laurensmalley15@gmail.com>
2020-11-26 11:54:27 -0800

    fixed club id issue

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -2,15 +2,20 @@ class RoomMessagesController < ApplicationController
   before_action :load_entities
 
   def create
-    @room_message = RoomMessage.create user: current_user,
+    @room_message = RoomMessage.new user: current_user,
                                        room: @room,
                                        message: params.dig(:room_message, :message)
     @club = @room.club
     authorize @room_message
-    RoomChannel.broadcast_to(
-      @room,
-      render_to_string(partial: "messages", locals: { message: @room_message })
-    )
+    if @room_message.save
+      redirect_to club_room_path(@club, @room, anchor: "message-#{@room_message.id}")
+      RoomChannel.broadcast_to(
+        @room,
+        render_to_string(partial: "messages", locals: { message: @room_message })
+      )
+    else
+      render "rooms/show"
+    end
   end
 
   protected
