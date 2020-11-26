1a65c3e0c37d092aa74b213e7c493a2e1e61a789
Diana Pinon <dianapinonb@gmail.com>
2020-11-26 13:33:51 -0700

    Repushing migration branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  add_flash_types :error, :success, :info
+  add_flash_types :error, :success, :info, :notification
 
   private
 
