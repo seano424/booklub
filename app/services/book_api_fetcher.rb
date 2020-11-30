e9bf20c3ba40488001ecd611211680a71480bfaa
Sean OReilly <soreilly424@gmail.com>
2020-11-30 09:45:11 -0500

    book cover images edited

--- app/services/book_api_fetcher.rb
+++ app/services/book_api_fetcher.rb
@@ -21,17 +21,17 @@ class BookApiFetcher
     document_serialized = open(google_url).read
     document            = JSON.parse(document_serialized)
     isbn                = document['items'][0]['volumeInfo']['industryIdentifiers'][0]['identifier']
-    # title               = document['items'][0]['volumeInfo']['title']
-    # authors               = document['items'][0]['volumeInfo']['authors']
-    # description         = document['items'][0]['volumeInfo']['description']
-    page_count         = document['items'][0]['volumeInfo']['pageCount']
-    categories         = document['items'][0]['volumeInfo']['categories']
-    avg_rating         = document['items'][0]['volumeInfo']['avgRating']
-    image_thumbnail         = document['items'][0]['volumeInfo']['imageLinks']['thumbnail']
+    # title             = document['items'][0]['volumeInfo']['title']
+    # authors           = document['items'][0]['volumeInfo']['authors']
+    # description       = document['items'][0]['volumeInfo']['description']
+    page_count          = document['items'][0]['volumeInfo']['pageCount']
+    categories          = document['items'][0]['volumeInfo']['categories']
+    avg_rating          = document['items'][0]['volumeInfo']['avgRating']
+    image     = document['items'][0]['volumeInfo']['imageLinks']['thumbnail']
 
-    image_sm = "http://covers.openlibrary.org/b/isbn/#{isbn}-S.jpg"
-    image_md = "http://covers.openlibrary.org/b/isbn/#{isbn}-M.jpg"
-    image_lg = "http://covers.openlibrary.org/b/isbn/#{isbn}-L.jpg"
+    # image_sm = "http://covers.openlibrary.org/b/isbn/#{isbn}-S.jpg"
+    # image_md = "http://covers.openlibrary.org/b/isbn/#{isbn}-M.jpg"
+    # image_lg = "http://covers.openlibrary.org/b/isbn/#{isbn}-L.jpg"
     {
       author: author,
       title: title,
@@ -39,11 +39,10 @@ class BookApiFetcher
       page_count: page_count, 
       categories: categories, 
       avg_rating: avg_rating, 
-      image_sm: image_sm,
-      image_md: image_md,
-      image_lg: image_lg,
-      image_thumbnail: image_thumbnail,
-      image_nil: image_nil
+      # image_sm: image_sm,
+      # image_md: image_md,
+      # image_lg: image_lg,
+      image: image
     }
   end
 end
