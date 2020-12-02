ff047326bf4400f79711eccd7b1c057cc7c26a8d
Lauren Smalley <laurensmalley15@gmail.com>
2020-12-02 10:06:57 -0800

    profile cards now link to proper club pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: true
-  validates :first_name, :last_name, presence:true
+  validates :first_name, :last_name, presence: true
 
   has_many :club_memberships, dependent: :destroy
   has_many :clubs, through: :club_memberships
