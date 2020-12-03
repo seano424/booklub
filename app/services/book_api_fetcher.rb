d1d5109db341bc9b08a3eeb0318e3ce491278f05
Sean OReilly <soreilly424@gmail.com>
2020-12-03 10:47:39 -0500

    Api reverted back to goodreads and final changes made

--- app/services/book_api_fetcher.rb
+++ app/services/book_api_fetcher.rb
@@ -5,42 +5,39 @@ class BookApiFetcher
   def initialize(id_value)
     @book_id = id_value
     @url = "https://www.goodreads.com/book/show/#{@book_id}.xml?key=#{ENV['GOODREADS_API_KEY']}"
+    
+    # google =  "https://books.googleapis.com/books/v1/volumes?q=20%2C000%20Leagues%20Under%20The%20Sea&filter=ebooks&orderBy=newest&printType=BOOKS&key=#{ENV['GOOGLE_API_KEY']}"
+    
   end
 
   def execute
     # Goodreads
     doc = Nokogiri::XML(URI.open(@url))
-    description = doc.search('description').first.text
-    image_url = doc.at('image_url').content
-    author = doc.search('name').first.text
+    description = doc.at('description').content
+    thumbnail = doc.at('image_url').content
+    author = doc.at('name').content
     title = doc.at('title').content
+    page_count = doc.at('num_pages').content
 
-    # Google 1
+    # Google to get ID
     book_title = title.gsub(" ", "%20")
     book_author = author.gsub(" ", "%20")
-    google_url          = "https://www.googleapis.com/books/v1/volumes?q=#{book_title}%20#{book_author}&langRestrict=en&key=#{ENV['GOOGLE_API_KEY']}"
+    google_url          = "https://www.googleapis.com/books/v1/volumes?q=#{book_title}%20#{book_author}&filter=ebooks&printType=BOOKS&langRestrict=en&key=#{ENV['GOOGLE_API_KEY']}"
     document_serialized = open(google_url).read
     document            = JSON.parse(document_serialized)
     book_id = document['items'][0]['id']
 
-    # Google 2
+    # Google Using the ID to get Categories
     id_url = "https://books.googleapis.com/books/v1/volumes/#{book_id}?key=#{ENV['GOOGLE_API_KEY']}"
     google_serialized = open(id_url).read
     google            = JSON.parse(google_serialized)
 
-    
-    # authors           = google['volumeInfo']['authors']
-    # description         = google['volumeInfo']['description']
-    page_count          = google['volumeInfo']['pageCount']
     categories          = google['volumeInfo']['categories']
     unless categories.nil?
       categories = categories.join(', ')
       categories = categories.gsub(' / ', ', ')
-      # categories = categories.gsub('Fiction, ', '')
-      # categories = categories.gsub('General, ', '')
     end
     avg_rating          = google['volumeInfo']['avgRating']
-    image               = google['volumeInfo']['imageLinks']['thumbnail']
     image_lg            = google['volumeInfo']['imageLinks']['large']
     image_md            = google['volumeInfo']['imageLinks']['medium']
     image_sm            = google['volumeInfo']['imageLinks']['small']
@@ -54,7 +51,7 @@ class BookApiFetcher
       image_sm: image_sm,
       image_md: image_md,
       image_lg: image_lg,
-      image: image
+      image: thumbnail
     }
   end
 end
