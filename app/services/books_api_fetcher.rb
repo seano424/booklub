f4911cec6b83b3f5f255a5c96bf5ef74cd7ef32b
Sean OReilly <soreilly424@gmail.com>
2020-12-03 22:39:38 -0500

    Strings normalized

--- app/services/books_api_fetcher.rb
+++ app/services/books_api_fetcher.rb
@@ -2,7 +2,9 @@ class BooksApiFetcher
   include Executable
 
   def initialize(book_query)
-    @book_query = book_query.gsub(" ", "+")
+    @book_query = book_query
+    normalize_query = @book_query.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n,'').downcase.to_s
+    @book_query = normalize_query.gsub(" ", "+")
     @url = "https://www.goodreads.com/search/index.xml?key=#{ENV['GOODREADS_API_KEY']}&q="
   end
 
@@ -10,7 +12,7 @@ class BooksApiFetcher
     search = @url + @book_query
     doc = Nokogiri::XML(URI.open(search)) 
     book = doc.xpath('//best_book').map do |best_book|  
-      author                = best_book.search('name').text.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n,'').downcase.to_s
+      author                = best_book.search('name')
       title                 = best_book.search('title').text
       # book_title          = title.gsub(" ", "%20")
       # book_author         = author.gsub(" ", "%20")
